7d2a650d1bfd446ca43eb56481d87a98f3838d38
larakode <larakodex@gmail.com>
2026-04-27 12:33:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,22 @@
-import os
+mport os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
-Token = os.getenv("8514363629:AAEjHDFCtAxeFgctSsrhsryIgrgQqKWDQeY")
+# TOKEN from Render environment
+TOKEN = os.getenv("8514363629:AAEjHDFCtAxeFgctSsrhsryIgrgQqKWDQeY")
+
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text.lower()
 
     if "hi" in text or "hello" in text:
-        msg = "Hey… you found me 😏"
+        msg = "Hey... you found me 😏"
     else:
-        msg = "You’re interesting… tell me more 😉"
+        msg = "You're interesting... tell me more 👀"
 
     await update.message.reply_text(msg)
 
 app = ApplicationBuilder().token(TOKEN).build()
+
 app.add_handler(MessageHandler(filters.TEXT, reply))
 
 print("Bot running...")
