13b4dc6da2e0b3f0716c516210722cb41421d4e7
larakode <larakodex@gmail.com>
2026-04-27 11:43:04 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,19 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+
+Token = "8514363629:AAEjHDFCtAxeFgctSsrhsryIgrgQqKWDQeY"
+async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text.lower()
+
+    if "hi" in text or "hello" in text:
+        msg = "Hey… you found me 😏"
+    else:
+        msg = "You’re interesting… tell me more 😉"
+
+    await update.message.reply_text(msg)
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(MessageHandler(filters.TEXT, reply))
+
+print("Bot running...")
+app.run_polling()
